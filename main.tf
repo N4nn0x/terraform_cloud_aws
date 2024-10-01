a39855e7b90ab58d2caa614cedc737bcdf3379a4
N4nn0x <119273133+N4nn0x@users.noreply.github.com>
2024-10-01 19:32:12 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,33 @@
 terraform {
   
 required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.74.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  tenant_id       = var.ARM_TENANT_ID
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/32"
+}
+
+
+
+
+
+
+
+
+/*
 
 resource "azurerm_resource_group" "mtc-rg" {
   name     = "mtc-resources"
@@ -23,7 +36,7 @@ resource "azurerm_resource_group" "mtc-rg" {
     environment = "Dev"
   }
 }
-/*
+
 resource "azurerm_virtual_network" "mtc-vn" {
   name                = "mtc-network"
   resource_group_name = azurerm_resource_group.mtc-rg.name
